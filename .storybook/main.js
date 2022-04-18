8d5e7f3a3870e30490444d96485400d3689b5637
Vanessa Yip <vanessa.yip@hingehealth.com>
2022-04-18 15:57:02 -0700

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
+  ],
+  features: {
+    postcss: false,
+  },
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack4",
+  },
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+import * as blah from '../src/index.css';
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
